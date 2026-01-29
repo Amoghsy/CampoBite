4f02b44da4219872862fe1be3563332ffad185e5
Amogh S Y <sy23amogh@gmail.com>
2026-01-29 18:55:20 +0530

    changes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -28,7 +28,7 @@ export default defineConfig(({ mode }) => ({
         start_url: "/",
         icons: [
           {
-            src: "/pwa-192x192.png",
+            src: "/src/assets/logo.png",
             sizes: "192x192",
             type: "image/png",
           },
