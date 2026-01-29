454922a09582b36e5d4c3e450d69aa55e811c4b3
Amogh S Y <sy23amogh@gmail.com>
2026-01-29 15:39:15 +0530

    changes

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
  // baseURL: "http://localhost:8082",
- baseURL: "https://campobite.onrender.com/",
+ baseURL: "https://campobite.onrender.com",
 });
 
 api.interceptors.request.use(config => {
